f6363495c103aaeecf4f1d7cf7825d009e64d590
Queisme <queiskey@gmail.com>
2024-07-25 15:51:23 -0500

    section 6 - The DOM

---
+++ 6. THE DOM - Document Object Model/1. intro to the Dom/script.js
@@ -0,0 +1,10 @@
+//console.log(window);
+// console.dir(window.document);
+// console.log(document.body.innerHTML);
+
+//console.log(document.links[0]);
+
+console.log(document.getElementById('main'));
+const main = document.getElementById('main');
+//main.innerHTML = '<h1>Hello From Main</h1>';
+document.querySelector('#main h1').innerText = 'Hello';
